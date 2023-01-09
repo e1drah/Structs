9bd85af37e341bf6c4510489d800b6cd1f381c75
e1drah <113189459+e1drah@users.noreply.github.com>
2023-01-09 15:17:48 -0400

    Added a vector2 struct

--- Structs/Program.cs
+++ Structs/Program.cs
@@ -8,8 +8,26 @@ namespace Structs
 {
     internal class Program
     {
+        // structs
+        struct vector2
+        {
+            public int x;
+            public int y;
+        }
+        static int x; //position
+        static int y; //position
+        static string playername; //name
+
         static void Main(string[] args)
         {
+            vector2 playerPosition;
+            playerPosition.x = 20;
+            playerPosition.y = 32;
+            Console.WriteLine("PlayerX: " + playerPosition.x + "PlayerY: " + playerPosition.y);
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
         }
     }
 }
